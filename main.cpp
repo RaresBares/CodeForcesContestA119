6e0edc1ffeb8880c922df9fc05ed130c04b94b8a
Rares <>
2020-04-03 17:20:07 +0200

    d

--- main.cpp
+++ main.cpp
@@ -5,19 +5,22 @@ using namespace std;
 int main() {
     int n, m;
 
-    cin >> n >> m;
+    cin >> n ;
+    cin >> m;
     vector<int> ls;
     while (m--){
         int i;
         cin >> i;
         ls.push_back(i);
     }
-
+    int i = 0;
 
     for(auto l = ls.begin(); l != ls.end(); l++){
-        cout << n-*l+1;
+        cout << n-*l+1 -i << "\n";
+        i++;
 
     }
+
 
 
 
